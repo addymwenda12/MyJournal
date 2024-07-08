0a14491af2d78338c938e4602591eaf6371e1c61
addymwenda12 <muneneaddy@gmail.com>
2024-07-08 15:38:46 +0300

    Configured journal routes in the start server file

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const { register, login } = require('./controllers/auth');
 const { authRoutes } = require('./routes/auth');
+const { journalRoutes } = require('./routes/journal');
+const verifyToken = require('./middleware/auth');
+
 const app = express();
 
 /* DATABASE SETUP */
@@ -8,6 +11,10 @@ const PORT = process.env.PORT || 3000;
 
 /* CONFIGURATION */
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use('/auth', authRoutes);
+app.use('/api/journals', verifyToken, journalRoutes);
+
 
 app.get('/', (req, res) => {
   res.send('Hello World');

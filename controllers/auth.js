f0c4a8422e21553385ed9d55fa0d7a8619606181
addymwenda12 <muneneaddy@gmail.com>
2024-07-08 14:42:45 +0300

    Added registration and login credentials for a user

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,31 +1,34 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-const mysqlConnection = require('../config/db')
-
+const mysqlConnection = require('../config/db');
 // import User from "../models/User.js";
 
 /* REGISTER USER */
 export const register = async (req, res) => {
   try {
-    const {
-      username,
-      password,
-      email
-    } = req.body;
+    const { username, password, email } = req.body;
 
     const salt = await bcrypt.genSalt();
     const passwordHash = await bcrypt.hash(password, salt);
 
-    mysqlConnection.query('INSERT INTO users (username, password, email) VALUES (?, ?, ?)', [
-      username,
-      passwordHash,
-      email
-    ], (error, results) => {
-      if (error) {
-        return res.status(500).send({ error: error.message });
-      }
-      res.status(201).send({ message: 'User registered successfully', userId: results.insertId });
-    });
+    // Wrap the MySQL query in a Promise to use async/await
+    const insertUser = () => {
+      return new Promise((resolve, reject) => {
+        mysqlConnection.query('INSERT INTO users (username, password, email) VALUES (?, ?, ?)', [username, passwordHash, email], (error, results) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(results);
+          }
+        });
+      });
+    };
+
+    const results = await insertUser();
+    res.status(201).send({ message: 'User registered successfully', userId: results.insertId });
+  } catch (error) {
+    // Catch any errors that occur during the process
+    res.status(500).send({ error: error.message });
   }
 };
 

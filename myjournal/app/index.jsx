643690db32782098f198489b2173cea06e709413
addymwenda12 <muneneaddy@gmail.com>
2024-07-09 09:15:29 +0300

    Confiured main file to import login and signup

---
+++ myjournal/app/index.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import AppNavigator from '../components/navigation/AuthStackNavigator';
+import * as Font from 'expo-font';
+import AppLoading from 'expo';
+import { isRequired } from 'react-native/Libraries/DeprecatedPropTypes/DeprecatedImageStylePropTypes';
+
+export default class App extends React.Component {
+  state = {
+    isFontLoaded: false
+  }
+
+  async componentDidMount(){
+    await Font.loadAsync({
+      'SemiBold': require('../assets/fonts/Montserrat-SemiBold.ttf'),
+      'Regular': require('../assets/fonts/Montserrat-Regular.ttf'),
+      'Medium': require('../assets/fonts/Montserrat-Medium.ttf'),
+    });
+    this.setState({ isFontLoaded: true });
+  }
+
+  render(){
+    return (
+      (this.state.isFontLoaded === true) ? (<AppNavigator />) : (<AppLoading />)
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
